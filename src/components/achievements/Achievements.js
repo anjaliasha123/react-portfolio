058e3e98b34e8af38bfc6c19cf95002145b6fc89
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2021-10-14 17:39:58 +0530

    components changes

---
+++ src/components/achievements/Achievements.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Achievements.scss";
+
+const Achievements = () => {
+  return <div className="achievements"></div>;
+};
+
+export default Achievements;
---
+++ src/components/survey/Survey.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Survey.scss";
+
+const Survey = () => {
+  return <div className="survey"></div>;
+};
+
+export default Survey;
