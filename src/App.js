7b22942eea8e9b6b7351559185cd7eb6d040c440
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2021-10-14 18:11:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./styles.css";
+import Topbar from "./components/topbar/Topbar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Technical from "./components/technical/Technical";
+import Achievements from "./components/achievements/Achievements";
+import Contact from "./components/contact/Contact";
+import Survey from "./components/survey/Survey";
 
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello CodeSandbox</h1>
+      <Topbar />
+      <div className="sections">
+        <Portfolio />
+        <Technical />
+        <Achievements />
+        <Survey />
+        <Contact />
+      </div>
     </div>
   );
 }
---
+++ src/components/technical/Technical.jsx
@@ -0,0 +1,8 @@
+import "./Technical.scss";
+import React from "react";
+
+const Technical = () => {
+  return <div className="technical"></div>;
+};
+
+export default Technical;
