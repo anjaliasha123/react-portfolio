932fce6e0d2ff824bff8da4a5cf1cec15351c025
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2021-10-15 20:52:44 +0530

    code changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Portfolio.scss";
+import Img from "../../img/img.png";
 
 const Portfolio = () => {
   return (
     <div className="portfolio" id="intro">
-      <div className="content">
+      <div className="left">
+        <img src={Img} alt="" />
+      </div>
+      <div className="right">
         <span className="name">Hi, I am Anjali Asha</span>
         <br />
         <span className="job">I am a developer</span>
-        <img src="https://i.gifer.com/BVLZ.gif" alt="" />
       </div>
     </div>
   );
